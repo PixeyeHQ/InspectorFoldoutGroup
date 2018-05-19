edb36f02db8702f86e4ba390b5c5f6c05a741558
Dimitry <dimmpixeye@users.noreply.github.com>
2018-05-19 11:52:01 +0200

    Add files via upload

--- Homebrew/DecorateFoldout/Attributes/FoldAttribute.cs
+++ Homebrew/DecorateFoldout/Attributes/FoldAttribute.cs
@@ -13,9 +13,14 @@ namespace Homebrew
 	public class FoldoutAttribute : PropertyAttribute
 	{
 		public string name;
-
-		public FoldoutAttribute(string name)
+		public bool foldEverything;
+ 
+		/// <summary>Adds the property to the specified foldout group.</summary>
+		/// <param name="name">Name of the foldout group.</param>
+		/// <param name="foldEverything">Toggle to put all properties to the specified group</param>
+		public FoldoutAttribute(string name, bool foldEverything = false)
 		{
+			this.foldEverything = foldEverything;
 			this.name = name;
 		}
 	}
